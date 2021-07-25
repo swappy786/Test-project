4e1d3f7d0dfe5bef03656f4cc7c3fe14863ec2af
Tenant <Tenant@Roshans-Laptop>
2021-07-25 12:30:00 +0530

    fourth commit Write Operation

--- ReadFromExcel/src/ddf/WriteFromExcel.java
+++ ReadFromExcel/src/ddf/WriteFromExcel.java
@@ -2,14 +2,46 @@ package ddf;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class WriteFromExcel {
 
 	public static void main(String[] args) throws IOException {
 		FileInputStream file = new FileInputStream(new File(System.getProperty("user.dir")+"\\input.xlsx"));
-		System.out.println("");
 		
+		//To initialize workbook   Opening file
+				XSSFWorkbook workbook = new XSSFWorkbook(file);
+				
+				//To initialize Sheet 1
+				XSSFSheet sheet = workbook.getSheetAt(0);
+				
+				// cell allocation
+				Cell write = sheet.getRow(0).getCell(0);// cell is allocated to update data
+				
+				// value set to particar Cell
+				
+				write.setCellValue("Techbodhi");
+				
+				//Read data
+				String fname = sheet.getRow(0).getCell(0).getStringCellValue();// to read data from particalr cell
+				
+				System.out.println(fname);
+				
+				// give the path of Output file 
+				
+				FileOutputStream filewrite = new FileOutputStream(new File(System.getProperty("user.dir")+"\\output.xlsx"));
+				
+				workbook.write(filewrite);
+		
+				filewrite.close();
+				
+				
+				
 	}
 
 }
